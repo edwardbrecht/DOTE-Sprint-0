ad6b16049557d2f852e15761965af4a7f60db618
Edward Cox Brecht <edbrecht@gmail.com>
2021-03-05 17:52:18 -0800

    Added simple player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float up;
+    private float right;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        up = Input.GetAxisRaw("Vertical");
+        right = Input.GetAxisRaw("Horizontal");
+
+        transform.TransformDirection(Vector3.forward);
+        transform.Translate(right * speed * Time.deltaTime, 0, up * speed * Time.deltaTime);
+
+
+    }
+}
